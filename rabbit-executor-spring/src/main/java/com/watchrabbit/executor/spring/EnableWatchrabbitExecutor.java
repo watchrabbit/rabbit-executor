637894ecd7fd6570e6ee494208482dee0c7fed5b
Mariusz <mariusz.luciow@cohesiva.com>
2014-09-30 22:53:22 +0200

    Initiated annotation config

--- rabbit-executor-spring/src/main/java/com/watchrabbit/executor/spring/EnableWatchrabbitExecutor.java
+++ rabbit-executor-spring/src/main/java/com/watchrabbit/executor/spring/EnableWatchrabbitExecutor.java
@@ -1,9 +1,20 @@
 package com.watchrabbit.executor.spring;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+
 /**
  *
  * @author Mariusz
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(WatchrabbitExecutorConfiguration.class)
+@Documented
 public @interface EnableWatchrabbitExecutor {
 
 }
---
+++ rabbit-executor-spring/src/main/java/com/watchrabbit/executor/spring/WatchrabbitExecutorConfiguration.java
@@ -0,0 +1,22 @@
+package com.watchrabbit.executor.spring;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
+import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProcessor;
+
+/**
+ *
+ * @author Mariusz
+ */
+@Configuration
+public class WatchrabbitExecutorConfiguration {
+
+    @Bean(name = AnnotationConfigUtils.SCHEDULED_ANNOTATION_PROCESSOR_BEAN_NAME)
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    public ScheduledAnnotationBeanPostProcessor scheduledAnnotationProcessor() {
+        return new ScheduledAnnotationBeanPostProcessor();
+    }
+}
